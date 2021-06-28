18faf43ba1596512c2d8f54fae7724e1e693f8ef
Callum <cwellkane@gmail.com>
2021-06-28 15:15:45 +1000

    removed commented out content and styling for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,8 @@
 import React from "react";
 import HeadMeta from "../components/HeadMeta";
 import Layout from "../components/layout";
-import styled from "@emotion/styled";
-import { centre_content } from "../components/helpers";
 import { graphql } from "gatsby";
 import Hero from "../components/globals/Hero";
-
-const Wrapper = styled.div`
-  ${centre_content.md};
-  margin: 4rem auto;
-`;
 
 export default function ContactPage({ location, data }) {
   const pageMeta = data.pageMeta.SEO;
@@ -28,22 +21,6 @@ export default function ContactPage({ location, data }) {
         }
         width={"md"}
       />
-      {/*<Wrapper>*/}
-      {/*  <h1*/}
-      {/*    css={css`*/}
-      {/*      font-size: var(--h3);*/}
-      {/*    `}*/}
-      {/*  >*/}
-      {/*    Interested in talking to us about a new project or want to find out*/}
-      {/*    more about our agency?*/}
-      {/*  </h1>*/}
-      {/*  <p>*/}
-      {/*    Please complete the form below or alternatively please feel free to*/}
-      {/*    email.*/}
-      {/*    <a href="mailto:hello@seamerdesign.com">hello@seamerdesign.com</a> or*/}
-      {/*    drop us a line.*/}
-      {/*  </p>*/}
-      {/*</Wrapper>*/}
     </Layout>
   );
 }
