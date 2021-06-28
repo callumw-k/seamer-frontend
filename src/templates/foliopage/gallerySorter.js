e245e070daed85829e8fdb611992a927f053c527
Callum <cwellkane@gmail.com>
2021-06-28 14:20:01 +1000

    fixed the gallery so images now either display as a 1:2 grid or a 1:1

--- src/templates/foliopage/gallerySorter.js
+++ src/templates/foliopage/gallerySorter.js
@@ -2,29 +2,28 @@ import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import styled from "@emotion/styled";
 import { breakpoints, centre_content } from "../../components/helpers";
-const isThreeColumn = `
-`;
+
 const ImageGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 1rem 1rem;
 
+  width: ${(props) => props.length};
   ${breakpoints.md} {
     grid-template-columns: 50% 50%;
     ${centre_content.xl};
-    div:nth-of-type(1) {
-      grid-column: 1/3;
+    & > div:first-of-type {
+      grid-column: ${(props) => (props.length ? "1/3" : "1/2")};
     }
   }
 `;
 export default function GallerySorter({ gallery, name }) {
   if (gallery) {
-    console.log(gallery.length === 3 ? "true" : "false");
+    let length = gallery.length === 3 ? 1 : 0;
     return (
-      <ImageGrid>
+      <ImageGrid length={length}>
         {gallery.map((image) => (
           <GatsbyImage
-            length={gallery.length}
             key={image.asset.id}
             alt={image.asset?.altText ? image.asset.altText : name}
             image={image.asset.gatsbyImageData}
