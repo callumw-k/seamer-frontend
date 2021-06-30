056879efbf715687a42dbcf3d3ac77c4db37d1cb
Callum Wellwod-Kane <cwellkane@gmail.com>
2021-06-30 20:52:33 +1000

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,39 +1,16 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { css } from "@emotion/react";
-import { centre_content } from "./helpers";
-import { StaticImage } from "gatsby-plugin-image";
-import ContactSection from "./footer/footer.ContactSection";
 
-const FooterWrapper = styled.footer`
-  background-color: #1b1b1b;
-  padding: 4rem 0;
+const Wrapper = styled.footer`
+  position: fixed;
+  bottom: 0;
+  height: 100vh;
+  overflow: hidden;
 `;
-
-const footerInner = css`
-  ${centre_content.md};
-`;
-
-export default function Footer({ location }) {
+export default function Footer() {
   return (
-    <React.Fragment>
-      <ContactSection location={location} />
-      <FooterWrapper>
-        <div css={footerInner}>
-          <StaticImage
-            css={css`
-              margin: 0 auto;
-              max-width: 300px;
-              display: block;
-            `}
-            imgStyle={{ objectFit: "contain" }}
-            width={300}
-            src="../images/seamer_logo.png"
-            alt="Seamer Design logo."
-            quality={100}
-          />
-        </div>
-      </FooterWrapper>
-    </React.Fragment>
+    <Wrapper>
+      <p>This is the footer</p>
+    </Wrapper>
   );
 }
